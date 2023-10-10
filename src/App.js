263f0b348663944fe61347ac5f56b8614cb69daf
Aray Akhmetova <99866973+comfort777888@users.noreply.github.com>
2023-10-10 17:38:08 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=881c5f6c";
+const API_URL = "https://www.omdbapi.com?apikey=881c5f6c";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
